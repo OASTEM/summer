e03cfa782165e534c6d00afede21df20a4410243
Brandon Nguyen <bnguyen9469@yahoo.com>
2016-06-11 13:44:49 -0700

    Hype it up

--- cal.js
+++ cal.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         events: {
             googleCalendarId: 'oastem.org_cmhe83nnmo1qtn68g1pc5q7pqk@group.calendar.google.com',
             className: 'gcal-event'
-        }
+        },
+        titleFormat: '[STEM Calendar:] MMMM YYYY'
     });
 });
